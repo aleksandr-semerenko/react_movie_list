df7a55e8e5a2234967150449fbb6b5f0c365996a
Aleksandr Semerenko <sanya96088475@gmail.com>
2021-08-10 16:16:21 +0300

    Updates

---
+++ precache-manifest.f8947bfa0d4b95af0b7ab8ea6f1d0962.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd3d3dd82aebd95cd308fd20781bf4f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9807c02642af6281c76b",
+    "url": "./static/css/2.a1df12c0.chunk.css"
+  },
+  {
+    "revision": "b52a527bf4d34d9ae1fc",
+    "url": "./static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "9807c02642af6281c76b",
+    "url": "./static/js/2.72de9324.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "./static/js/2.72de9324.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b52a527bf4d34d9ae1fc",
+    "url": "./static/js/main.7de48fab.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
